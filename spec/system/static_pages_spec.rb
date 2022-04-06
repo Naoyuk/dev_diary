2dd31c4c9661e1ab20c43ce6d5b7823434e77c05
Naoyuki Ishida <39.ishida@gmail.com>
2022-04-05 22:09:02 -0700

    create full_title helper method and enable to use it in rspec

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -8,14 +8,28 @@ RSpec.describe "StaticPages", type: :system do
   scenario "you can visit home page" do
     visit root_path
 
-    expect(page).to have_title "Home | DevDiary"
+    expect(page).to have_title full_title("Home")
     expect(page).to have_content "Find me in app/views/static_pages/home.html.erb"
   end
 
   scenario "you can visit about page" do
     visit static_pages_about_path
 
-    expect(page).to have_title "About | DevDiary"
+    expect(page).to have_title full_title("About")
     expect(page).to have_content "このWebサイトでは、主にアプリケーションの開発日誌を書き留めていきます。"
+  end
+
+  scenario "can jump to about page from home" do
+    visit static_pages_home_path
+
+    click_link "About"
+    expect(page).to have_title full_title("About")
+  end
+
+  scenario "can jump to home page from about" do
+    visit static_pages_about_path
+
+    click_link "Home"
+    expect(page).to have_title full_title("Home")
   end
 end
