53e4ee25dd062c93186634016795110bbe9c3ec5
Naoyuki Ishida <39.ishida@gmail.com>
2022-04-06 21:00:46 -0700

    implement sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
-  root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :users
 end
