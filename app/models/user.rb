d30cc666cf5302b3afe20ceb2704c1f616a0ffb2
Naoyuki Ishida <39.ishida@gmail.com>
2022-04-12 23:19:28 -0700

    implement permanent session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Userインスタンスと記憶トークンを紐付けるための仮想属性
+  attr_accessor :remember_token
+
   # Associations
 
   # Callback
@@ -21,6 +24,36 @@ class User < ApplicationRecord
 
   # Secure password
   has_secure_password
+
+  # User class methods
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # User instance methods
+  # 記憶トークンをUserインスタンスに紐付け、ハッシュ化してremember_digestに保存
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 引数として渡したトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
   # methods
   private
