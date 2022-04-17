161f2357d526e65e4fb0768d5f703eeb64daa7fe
Naoyuki Ishida <39.ishida@gmail.com>
2022-04-16 23:44:12 -0700

    implement edit user profile

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
-module TestHelper
-  # テストユーザーがログイン中の場合にtrueを返す
-  def is_logged_in?
-    !session[:user_id].nil?
+module SystemHelper
+  def login_as(user)
+    visit login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
   end
+end
+
+RSpec.configure do |config|
+  config.include SystemHelper
 end
--- spec/system/user_edits_spec.rb
+++ spec/system/user_edits_spec.rb
@@ -1,19 +1,25 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe 'UserSignups', type: :system do
+RSpec.describe "UserEdits", type: :system do
   before do
     driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
   end
 
-  it 'is invalid with no informations' do
-    visit signup_path
+  it "requires to be logged in" do
+    visit edit_user_path(@user)
+    expect(page).not_to have_content 'Name'
+    expect(page).not_to have_content 'Confirmation'
+  end
+
+  it "is invalid with no informations" do
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: ''
     fill_in 'Email', with: ''
     fill_in 'Password', with: ''
     fill_in 'Confirmation', with: ''
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content "Name can't be blank"
     expect(page).to have_content "Email can't be blank"
@@ -21,69 +27,82 @@ RSpec.describe 'UserSignups', type: :system do
     expect(page).to have_content "Password confirmation can't be blank"
   end
 
-  it 'is invalid with too long name' do
-    visit signup_path
+  it "is invalid with a too long name" do
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: 'a' * 51
     fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Confirmation', with: 'password'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content 'Name is too long (maximum is 50 characters)'
   end
 
-  it 'is invalid with too long email' do
-    visit signup_path
+  it "is invalid with a too long email" do
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: 'test'
     fill_in 'Email', with: "#{'a' * 244}@example.com"
     fill_in 'Password', with: 'password'
     fill_in 'Confirmation', with: 'password'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content 'Email is too long (maximum is 255 characters)'
   end
 
-  it 'is invalid with invalid email address' do
-    visit signup_path
+  it "is invalid with an invalid email address" do
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: 'test'
     fill_in 'Email', with: 'invalid@email@address.com'
     fill_in 'Password', with: 'password'
     fill_in 'Confirmation', with: 'password'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content 'Email is invalid'
   end
 
   it 'is invalid with differ between Password and Confirmation' do
-    visit signup_path
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: 'test'
     fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Confirmation', with: 'abcdef'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content "Password confirmation doesn't match Password"
   end
 
   it 'is invalid with too short password' do
-    visit signup_path
+    login_as(@user)
+    visit edit_user_path(@user)
     fill_in 'Name', with: 'test'
     fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'pass'
     fill_in 'Confirmation', with: 'pass'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
     expect(page).to have_content 'Password is too short (minimum is 6 characters)'
   end
 
   it 'is valid with correct informations' do
-    visit signup_path
-    fill_in 'Name', with: 'test'
-    fill_in 'Email', with: 'testemail@example.com'
+    login_as(@user)
+    visit edit_user_path(@user)
+    name = 'new_name'
+    email = 'new_email@example.com'
+    fill_in 'Name', with: name
+    fill_in 'Email', with: email
     fill_in 'Password', with: 'password'
     fill_in 'Confirmation', with: 'password'
-    click_button 'Create My Account'
+    click_button 'Save Changes'
 
-    expect(page).to have_content 'Welcome to DevDiary!'
+    expect(page).to have_content 'Your profile has updated!'
+    expect(page).to have_content name
+    @user.reload
+    expect(@user.name).to eq name
+    expect(@user.email).to eq email
   end
+
 end
